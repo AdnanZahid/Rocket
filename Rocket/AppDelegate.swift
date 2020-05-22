96e1dd7d6c7ac9b387e40bbde794e9df2f91454e
Adnan Zahid <adnan.zahid@dubizzle.com>
2020-05-22 10:58:59 +0400

    Initial Commit

---
+++ Rocket/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Rocket
+//
+//  Created by Adnan Zahid on 22/05/2020.
+//  Copyright © 2020 Rocket. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
